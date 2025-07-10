ebcbf75edbc6c410e56c109ff06ea8577eac2771
Ludicard <sergio.masin@udla.edu.ec>
2025-07-10 07:25:43 -0500

    Creado Modelo Producto con los tipos de datos necesarios

---
+++ MasinExamenProgreso3/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+namespace MasinExamenProgreso3.Models
+{
+    [Table("productos")]
+    public class Producto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        [MaxLength(50)]
+        public string Categoria { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal PrecioEstimado { get; set; }
+    }
+}
