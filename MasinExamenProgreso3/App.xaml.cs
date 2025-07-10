c11de55dd7fc36329addf599004294f29c850751
Ludicard <sergio.masin@udla.edu.ec>
2025-07-10 08:01:22 -0500

    Creado el Servicio del LOG y conectado con la app

--- MasinExamenProgreso3/App.xaml.cs
+++ MasinExamenProgreso3/App.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace MasinExamenProgreso3
+﻿using MasinExamenProgreso3.Data;
+
+namespace MasinExamenProgreso3
 {
     public partial class App : Application
     {
-        public App()
+        public static ProductoRepository ProductoRepo { get; private set; }
+        public static LogService LogServicio { get; private set; }
+
+        public App(ProductoRepository repo, LogService logService)
         {
             InitializeComponent();
+
+            ProductoRepo = repo;
+            LogServicio = logService;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
---
+++ MasinExamenProgreso3/LogService.cs
@@ -0,0 +1,27 @@
+﻿namespace MasinExamenProgreso3.Data
+{
+    public class LogService
+    {
+        private readonly string _logFilePath;
+
+        public LogService(string apellido)
+        {
+            string fileName = $"Logs_{apellido}.txt";
+            _logFilePath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+        }
+
+        public void EscribirLog(string nombreProducto)
+        {
+            string log = $"Se incluyó el registro [{nombreProducto}] el {DateTime.Now:dd/MM/yyyy HH:mm}";
+            File.AppendAllText(_logFilePath, log + Environment.NewLine);
+        }
+
+        public List<string> ObtenerLogs()
+        {
+            if (!File.Exists(_logFilePath))
+                return new List<string>();
+
+            return File.ReadAllLines(_logFilePath).ToList();
+        }
+    }
+}
